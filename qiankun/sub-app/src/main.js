df1cbf36c1f5f2e5f3ad88df66971b535922b334
ljylwnxx <ljylwnxx@163.com>
2023-03-31 14:10:42 +0800

    feat:子应用

--- qiankun/sub-app/src/main.js
+++ qiankun/sub-app/src/main.js
@@ -1,8 +1,53 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+import routes from "./router"
+import store from "./store"
+import VueRouter from "vue-router"
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+// 微前端 - 子应用配置
+let router = null
+let instance = null
+
+if (window.__POWERED_BY_QIANKUN__) {
+  // eslint-disable-next-line
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
+}
+
+function render(props = {}) {
+  const { container } = props
+  router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? "/sub-app/" : "/", // 抛出路由加前缀
+    mode: "history",
+    routes
+  })
+
+
+  instance = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount(container ? container.querySelector("#app") : "#app")
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render()
+}
+export default instance
+
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped")
+}
+
+export async function mount(props) {
+  // props 包含主应用传递的参数  也包括为子应用 创建的节点信息
+  render(props)
+}
+
+export async function unmount() {
+  instance.$destroy()
+  instance.$el.innerHTML = ''
+  instance = null
+}
---
+++ qiankun/sub-app/src/views/sub-app/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="sub-app">
+      我是子应用
+    </div>
+  </template>
+  
+  <style lang="scss" scoped>
+  .sub-app {
+    cursor: pointer;
+    background-color: aqua;
+  }
+  </style>
