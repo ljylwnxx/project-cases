e3169b3b2a60192607d4ec9c2ca45298f5abd732
ljylwnxx <ljylwnxx@163.com>
2023-03-31 13:57:58 +0800

    feat:主应用

---
+++ qiankun/main-app/src/router/index.js
@@ -0,0 +1,5 @@
+{
+    path: "/sub-app/*",
+    meta: { title: "子应用" },
+    component: () => import("@/views/qiankun/index")
+}
---
+++ qiankun/main-app/src/views/qiankun/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="subapp-viewport"></div>
+  </template>
+  
+  <script>
+  import start from "@/qiankun/index";
+  export default {
+    mounted() {
+      // 启动微前端
+      if (!window.qiankunStarted) {
+        window.qiankunStarted = true;
+        start();
+      }
+    }
+  };
+  </script>
