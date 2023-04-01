32b8d6638be9c9a09133ada74c7587f9334e5551
ljylwnxx <ljylwnxx@163.com>
2023-04-01 15:38:07 +0800

    fix:路由问题

--- qiankun/sub-app/src/App.vue
+++ qiankun/sub-app/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-	<!-- <div id="app">
-	  <div id="nav">
-		<router-link to="/Music">Music</router-link>
-		<router-link to="/Dome">About</router-link>
-		<router-link to="/about">MVVM</router-link>
-	  </div>
-	  <router-view/>
-	</div> -->
-  </template>
-  
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link>
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view />
+	</div>
+</template>
+
 <script>
 
 </script>
--- qiankun/sub-app/src/views/Home.vue
+++ qiankun/sub-app/src/views/Home.vue
@@ -2,6 +2,11 @@
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png" />
 		<h1 style="color:red;">This is a home page in sub-app</h1>
-		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
+<script>
+
+</script>
+
+<style>
+</style>
