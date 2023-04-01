490ea01115ee3501241e816a7fbd9e077bc43acb
ljylwnxx <ljylwnxx@163.com>
2023-04-01 11:46:34 +0800

    feat:main-app

--- qiankun/main-app/src/qiankun/index.js
+++ qiankun/main-app/src/qiankun/index.js
@@ -1,5 +1,5 @@
 import { registerMicroApps, start } from "qiankun";
-import store from "@/store";
+// import store from "@/store";
 
 
 registerMicroApps([
@@ -8,10 +8,10 @@ registerMicroApps([
     entry: "http://localhost:8080", // 微应用入口
     container: "#subapp-viewport", // 微应用挂载的div
     activeRule: "/sub-app/",
-    props: {
-      // 此处将父应用的 store 传入子应用
-      store
-    }
+    // props: {
+    //   // 此处将父应用的 store 传入子应用
+    //   store
+    // }
   }
 ])
 
