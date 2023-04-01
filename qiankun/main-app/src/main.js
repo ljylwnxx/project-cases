aa967143f3badcbe42cb5ad14d922e0b4cb05ab4
ljylwnxx <ljylwnxx@163.com>
2023-04-01 17:27:02 +0800

    fix

--- qiankun/main-app/src/main.js
+++ qiankun/main-app/src/main.js
@@ -5,8 +5,8 @@ import router from './router'
 import {registerMicroApps, start} from 'qiankun' //新增部分，导入qiankun中的两个方法
 const apps = [
 {
-	name:'sub-app', //子应用的名称
-	entry:'//localhost:8080',//子应用的域名
+	name:'subApp', //子应用的名称
+	entry:'//localhost:8081',//子应用的域名
 	container:'#vueContainer',//承载子应用的容器，在上面App.vue中定义
 	activeRule:'/subapp', // 被激活的子应用的路由
 }
