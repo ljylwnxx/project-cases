e70b6a33628ffa1602079c0e3c400950d367269d
ljylwnxx <ljylwnxx@163.com>
2023-04-01 14:44:06 +0800

    feat:main-app

--- qiankun/main-app/src/router/index.js
+++ qiankun/main-app/src/router/index.js
@@ -1,6 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from "../views/Home";
+import About from "../views/About";
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        // redirect: '/home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+
+]
 //...省略原有不需修改的代码，以下是修改后的代码
 const router = new VueRouter({
 	mode:'history',
 	base: '',
 	routes
 })
+export default router
