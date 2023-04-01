79a41a2b472fbc91e3c139dcb1a5d8eb74411982
ljylwnxx <ljylwnxx@163.com>
2023-04-01 14:52:55 +0800

    fix:vue-router

--- qiankun/sub-app/src/router/index.js
+++ qiankun/sub-app/src/router/index.js
@@ -1,5 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from "../views/Home";
+import About from "../views/About";
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        // redirect: '/home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+
+]
 const router = new VueRouter({
 	mode:'history',
 	base:'/sub-app',
 	routes
 })
+export default router
